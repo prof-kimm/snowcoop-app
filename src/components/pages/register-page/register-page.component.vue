2f3387faf5634d31f74065d21d064358a8e59482
prof-kimm <prof-kimm@algonquincollege.com>
2019-03-07 10:20:34 -0500

    Snowcoop-app

---
+++ src/components/pages/register-page/register-page.component.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="registerPage">
+    <div class="create-account-box-wrapper">
+      <el-card class="create-account-box">
+        <div class="form-header-wrapper">
+          <!-- TODO: add title -->
+          <span class="form-title-text">Create your account</span>
+        </div>
+        <div class="form-body-wrapper">
+          <!-- TODO: add form and form items inside for all control -->
+          <div class="register-form">
+            <el-form :model="formData" ref="formData" @focusout.native="updateIsFormValidated">
+              <el-row :gutter="20">
+                <el-col :span="12">
+                  <el-form-item :rules="rules.firstName" prop="firstName">
+                    <el-input placeholder="First name" v-model="formData.firstName"></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :rules="rules.lastName" prop="lastName">
+                    <el-input placeholder="Last name" v-model="formData.lastName"></el-input>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+              <el-row>
+                <el-form-item :rules="rules.email" prop="email">
+                    <el-input placeholder="Email" v-model="formData.email"></el-input>
+                </el-form-item>
+              </el-row>
+              <el-row :gutter="20">
+                <el-col :span="12">
+                  <el-form-item :rules="rules.password" prop="password">
+                    <el-input type="password" placeholder="Password" v-model="formData.password"></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item :rules="rules.passwordConfirm" prop="passwordConfirm">
+                    <el-input type="password" placeholder="Confirm" v-model="formData.passwordConfirm"></el-input>
+                  </el-form-item>                
+                </el-col>
+              </el-row>
+            </el-form>
+          </div>
+          <div class="instruction-text-wrapper">
+            <span class="instruction-text">
+              Use 8 or more characters with a mix of letters, numbers & symbols
+            </span>
+          </div>
+        </div>
+        <div class="form-footer-wrapper">
+          <!-- TODO: add buttons and wire up event handler and routing method -->
+          <div class="text-button-wrapper">
+            <router-link to="/login">
+              <a>Sign in instead</a>
+            </router-link>
+          </div>
+          <div class="next-button-wrapper">
+            <button v-bind:disabled="!isFormValidated" @click="register">Next</button>
+          </div>
+        </div>
+      </el-card>
+    </div>
+  </div>
+</template>
+<script src="./register-page.component.js"></script>
+<style src="./register-page.component.css"></style>
---
+++ src/router.js
@@ -0,0 +1,32 @@
+import VueRouter from 'vue-router';
+import loginPage from './components/pages/login-page/login-page.component.vue';
+import registerPage from './components/pages/register-page/register-page.component.vue';
+import dashboardPage from './components/pages/dashboard-page/dashboard-page.component.vue';
+import listPage from './components/pages/list-page/list-page.component.vue';
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: loginPage
+    },
+    {
+      path: '/register',
+      component: registerPage
+    },
+    {
+      path: '/dashboard',
+      component: dashboardPage
+    },
+        {
+      path: '/list',
+      component: listPage
+    }
+  ]
+});
+
+export default router;
