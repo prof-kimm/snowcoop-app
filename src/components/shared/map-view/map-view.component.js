37c74e5f85dd156c40c61c07c9a32434de02ef7e
madkimchi <minkim.dev@gmail.com>
2019-03-20 00:28:21 -0400

    adding map-view component

---
+++ src/components/shared/map-view/map-view.component.js
@@ -0,0 +1,6 @@
+export default {
+  name: 'MapView',
+  data() {
+    return { };
+  },
+};
---
+++ src/components/shared/map-view/map-view.component.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="mapView">Map View Component</div>
+</template>
+
+<script src="./map-view.component.js"></script>
+<style src="./map-view.component.css" scoped></style>
