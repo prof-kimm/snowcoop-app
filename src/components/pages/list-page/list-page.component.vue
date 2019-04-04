bf3f3ff526936fa2cdead114db2994f7d77cc2d3
madkimchi <minkim.dev@gmail.com>
2019-04-04 12:45:37 -0400

    adding map-view component

--- src/components/pages/list-page/list-page.component.vue
+++ src/components/pages/list-page/list-page.component.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="listPage">list page</div>
+  <div id="listPage">
+    <div class="list-wrapper">
+      <div class="map-view-wrapper">
+        <map-view
+          v-if="addressList"
+          v-bind:showList="false"
+          v-bind:addressList="addressList">
+        </map-view>
+      </div>
+    </div>
+  </div>
 </template>
 <script src="./list-page.component.js"></script>
 <style src="./list-page.component.css"></style>
